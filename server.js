e12095a45950c8a193ae72096f98da44416dd10d
MikinjMistry <mikinj-mistry@hotmail.com>
2019-08-31 16:08:24 +0530

    chrome location changes for ubuntu

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
-var server = prerender();
+var server
+if (process.platform !== 'win32') {
+    server = prerender({
+        chromeLocation: '/home/ubuntu/beta-webapp/node_modules/puppeteer/.local-chromium/linux-656675/chrome-linux/chrome'
+    });
+} else {
+    server = prerender();
+}
 
 server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.blockResources());
